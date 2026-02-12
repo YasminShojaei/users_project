fc6a3e638b5d2ff7ab82b9e1c3ce6c76923b74c8
YasminShojaei <yasminshojaeii@gmail.com>
2026-02-13 02:08:54 +0330

    main programm added

--- view/user_app.py
+++ view/user_app.py
@@ -1,13 +1,26 @@
 from controller import user_controller
 
 
-def save_user(self):
-    user_id = self.user_id.get()
-    username = self.username.get()
-    password = self.password.get()
+class MainProgramm:
 
-    u_controller = UserController()
-    result = u_controller.save_user(user_id, username, password)
-    if result:
-        print('User saved')
+    def __init__(self):
+        pass
+
+    def menu(self):
+
+        print("_____________________")
+        print('Welcome to User App')
+        print("1.Enter data base")
+        print("2.Exit")
+        print("_____________________")
+
+        user_input = input()
+
+
+
+
+
+
+
+
 
